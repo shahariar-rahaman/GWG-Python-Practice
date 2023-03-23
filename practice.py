0fdc895c4a3df95b728c4c8b425a490b7e6be79d
Md. Shahariar Rahaman <shahariar15-2112@diu.edu.bd>
2023-03-24 03:27:25 +0600

    another explict conversion

--- practice.py
+++ practice.py
@@ -15,10 +15,16 @@ for x in range(10):
 print(type(7))
 print(type("S"))
 print(type(0.7))
-"""
+
 #implicit conversion 
 print(7+8.5)
 #explicit conversion
 a = 10
 b = "20"
 print(a+int(b))
+
+#In python string and interger or flot type data not allowed to merge.Here we need make explict operation.
+a = 10
+b = "Sting"
+print(b +" "+ str(a))
+"""
