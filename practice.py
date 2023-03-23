862032e135461f5ba6ccf16d1955d1854008e667
Md. Shahariar Rahaman <shahariar15-2112@diu.edu.bd>
2023-03-24 03:16:01 +0600

    range function

--- practice.py
+++ practice.py
@@ -1,7 +1,13 @@
 #Hello World Python
+"""
 print("Hello World!")
 
 #Python list of loop
 names = ["Shahariar","Arif","Ratul","Adnan"]
 for name in names:
     print(name)
+    
+#range function
+for x in range(10):
+    print(x)
+"""
